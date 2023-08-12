402d9dc310d5a8cfa02eed609f1610b302422989
Andrew <andrew.shedov@gmail.com>
2023-08-13 01:57:07 +0300

    commit.

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -1,7 +1,7 @@
 import styles from "./modal.module.css";
 import { useState, useEffect, useRef } from "react";
-import CloseIcon from "../svg/closeicon/closeicon";
-import Horse from "../svg/Horse/Horse";
+import CloseIcon from "../svg/CloseIcon/index";
+import Horse from "../svg/Horse/index";
 export default function Modal({buttonShowModal, setButtonShowModal}) {
   const [showModal, setShowModal] = useState(false);
   useEffect(() => {
